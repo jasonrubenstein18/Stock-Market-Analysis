088366b117906f315f538b9b131932ae1bf93d25
Jason Rubenstein <jasonr34@outlook.com>
2020-01-10 08:35:43 -0500

    Update StockPullYahoo.py

--- StockPullYahoo.py
+++ StockPullYahoo.py
@@ -64,7 +64,7 @@ stock_data = get_stock_data(t1, stocks_start, stocks_end)#.reset_index()
 Create sqlite3 database for data storage?
 """
 
-stock_data.to_csv('~/Python/QuantFin1/StockData/stock_data.csv', index=0)
+stock_data.to_csv('...', index=0)
 
 
 end_time = time()
