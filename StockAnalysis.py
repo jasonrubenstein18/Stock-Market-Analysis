e3228810f05f9a60ac623f4e78efc5ac8d801e82
Jason Rubenstein <jasonr34@outlook.com>
2020-01-16 17:38:07 -0500

    Update StockAnalysis.py

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -4,6 +4,8 @@ Created on Wed Dec 18 11:20:22 2019
 @author: jasonrubenstein
 
 ESN plot from: https://github.com/mrdragonbear/EchoStateNetworks/blob/master/Echo_State_Networks.ipynb
+
+remember to update path in row 49
 """
 
 import datetime
