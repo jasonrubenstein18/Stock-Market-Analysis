3e4f1724d135a176236ae7fa668071a1f1182f8c
Jason Rubenstein <jasonr34@outlook.com>
2021-01-17 22:03:57 -0500

    Update StockPullYahoo_csv.py

--- StockPullYahoo_csv.py
+++ StockPullYahoo_csv.py
@@ -53,10 +53,6 @@ start_time = time()
 stock_data = get_stock_data(t1, stocks_start, stocks_end)#.reset_index()
 
 
-"""
-Create sqlite3 database for data storage?
-"""
-
 stock_data.to_csv('/Users/jasonrubenstein/Desktop/Python/QuantFin1/StockData/stock_data'
                   + str(stock_data['Date'].max()) + '.csv', index=0)
 
